3747214f6441c0f23322e282c8a37bc2af423996
workfatima829 <workfatima829@gmail.com>
2025-11-20 18:52:27 +0500

    use the router in app routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ArticleDetailsPage from "../features/news/pages/ArticleDetailsPage";
 import NewsAPIPage from "../features/news/pages/NewsAPIPage";
 
 export const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<NewsAPIPage/>} />
-  <Route path="/article/:id" element={<ArticleDetailsPage />} />
-  </Routes>
+  <Router>
+    <Routes>
+      <Route path="/" element={<NewsAPIPage />} />
+      <Route path="/article/:id" element={<ArticleDetailsPage />} />
+    </Routes>
+  </Router>
 );
